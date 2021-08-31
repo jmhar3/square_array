ac80abcedf3f592f943a911382633dfc646048d7
jmhar <jmhar@protonmail.com>
2021-08-31 17:00:20 +1000

    square

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,15 @@
 def square_array(array)
-  # your code here
+  new_array = []
+  array.each do |number|
+    new_array << number ** 2
+  end
+  new_array
 end
+
+num_array=[1,2,3]
+
+def other_square_array(array)
+  array.collect { |number| number ** 2 }
+end
+
+other_square_array(num_array)
